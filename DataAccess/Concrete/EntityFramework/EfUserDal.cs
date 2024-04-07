dfae28b7816a92212c836d5d3a36cd3e7877cf5e
Okyarilkay <107274565+Okyarilkay@users.noreply.github.com>
2024-04-08 02:00:56 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -0,0 +1,17 @@
+﻿using Core.DataAccess;
+using DataAccess.Abstract;
+using DataAccess.Concrete.Context;
+using DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfUserDal : EfEntityRepository<User, SocialMediaContext>, IUserDal
+    {
+
+    }
+}
---
+++ Entities/Concrete/Comment.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using System.Security.Principal;
+
+namespace Entities.Concrete
+{
+    public class Comment : IEntity
+    {
+        public int Id { get; set; }
+        public int ArticleId { get; set; }
+        public int UserId { get; set; }
+        public string CommentText { get; set; }
+        public DateTime CommentDate { get; set; }
+    }
+}
