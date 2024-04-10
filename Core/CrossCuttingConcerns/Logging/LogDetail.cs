ed0b3087c11bc3fe1b32b2ba19380ee11b538d0c
Okyarilkay <107274565+Okyarilkay@users.noreply.github.com>
2024-04-11 01:27:27 +0300

    Caching(MicrosoftMemoryCache), Logging(log4net), Intercetors are done.

---
+++ Core/CrossCuttingConcerns/Logging/LogDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Logging
+{
+    public class LogDetail
+    {
+        public string methodName { get; set; }
+        public List<LogParameter> logParameters { get; set; }
+    }
+}
---
+++ Core/Extensions/Exception/ExceptionMiddleware.cs
@@ -0,0 +1,71 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Extensions.Exception
+{
+    public class ExceptionMiddleware
+    {
+        RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (System.Exception e)
+            {
+
+                await HandleExceptionAsync(context,e);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, System.Exception e)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            string message = "Internal Server Error";
+
+            if (e.GetType() == typeof(ValidationException))
+            {
+                message = "Bad Request";
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                IEnumerable<ValidationFailure> validationErrors = ((ValidationException)e).Errors;
+                return context.Response.WriteAsync(new ValidationErrorDetails()
+                {
+                    StatusCode = 400,
+                    Message = message,
+                    ValidationErrors = validationErrors
+                }.ToString());
+            }
+
+            if (e.GetType() == typeof(UnauthorizedAccessException))
+            {
+                message = "Unauthorized.";
+                context.Response.StatusCode= (int)HttpStatusCode.Unauthorized;
+                return context.Response.WriteAsync(new ErrorDetails()
+                {
+                    StatusCode = context.Response.StatusCode,
+                    Message = message,
+                }.ToString());
+            }
+            return context.Response.WriteAsync(new ErrorDetails
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = message,
+            }.ToString());
+        }
+    }
+}
